0384282def4cdaa984e405ca1cef35cf4766ec0f
ecsbeats <snakebiking49@gmail.com>
2021-05-18 13:05:29 -0700

    Update README.md, create packages, and create tests.

---
+++ tests/tests.py
@@ -0,0 +1,17 @@
+import unittest
+from futurenode import app
+
+
+class Flask(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    def test_getIndex(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
